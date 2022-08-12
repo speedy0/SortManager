897c187cb49c9242bf5efa0e3a3b7c6d0f24024d
Aleksander Napieralski <aleksander.napieralski@city.ac.uk>
2022-08-12 10:41:33 +0100

    Bugs for MergeSort tests fixed.

--- src/test/java/com/sparta/an/sortingAlgorithms/MergeSortTest.java
+++ src/test/java/com/sparta/an/sortingAlgorithms/MergeSortTest.java
@@ -10,32 +10,32 @@ public class MergeSortTest {
     @Test
     @DisplayName("Is sorting in correct numerical order")
     void isSortingInCorrectNumericalOrder(){
-        int[] randNumbers = {5, 3, 0, 9, 11, 17, 404, 50, 58, 36, 17, 52, 5};
-        int[] expected = {0, 3, 5, 5, 9, 11, 17, 17, 36, 50, 52, 58, 404};
+        int[] randNumbers = {5, 3, 0, 9, 11, 17, 404, 50, 58, 36, 17, 52, 5, 3};
+        int[] expected = {0, 3, 3, 5, 5, 9, 11, 17, 17, 36, 50, 52, 58, 404};
         Assertions.assertEquals(Arrays.toString(expected),Arrays.toString(new MergeSort().sort(randNumbers)));
     }
 
     @Test
     @DisplayName("When passing negative values, returns numerical order.")
     void whenPassingNegativeValuesReturnsNumericalOrder(){
-        int[] randNumbers = {5, -1, -5, 9, 11, 17, 404, 50, 58, 36, 17, 52, 0};
-        int[] expected = {-5, -1, 0, 5, 9, 11, 17, 17, 36, 50, 52, 58, 404};
+        int[] randNumbers = {5, -1, -5, 9, 11, 17, 404, 50, 58, 36, 17, 52, 0, 4};
+        int[] expected = {-5, -1, 0, 4, 5, 9, 11, 17, 17, 36, 50, 52, 58, 404};
         Assertions.assertEquals(Arrays.toString(expected),Arrays.toString(new MergeSort().sort(randNumbers)));
     }
 
     @Test
     @DisplayName("When passing duplicates, returns sorted array")
     void whenPassingDuplicatesReturnsSortedArray(){
-        int[] randNumbers = {0, -5, -5, 9, 11, 17, 404, 50, 58, 36, 17, 52, 0};
-        int[] expected = {-5, -5, 0, 0, 9, 11, 17, 17, 36, 50, 52, 58, 404};
+        int[] randNumbers = {0, -5, -5, 9, 11, 17, 404, 50, 58, 36, 17, 52, 0, 1};
+        int[] expected = {-5, -5, 0, 0, 1, 9, 11, 17, 17, 36, 50, 52, 58, 404};
         Assertions.assertEquals(Arrays.toString(expected),Arrays.toString(new MergeSort().sort(randNumbers)));
     }
 
     @Test
     @DisplayName("When passed an array, returns correct length of the array back")
     void whenPassedAnArrayReturnsCorrectLengthOfTheArrayBack(){
-        int[] randNumbers = {3, -5, -5, 9, 11, 17, 404, 50, 58, 36, 17, 52, 0};
-        int[] expected = {-5, -5, 3, 0, 9, 11, 17, 17, 36, 50, 52, 58, 404};
+        int[] randNumbers = {3, -5, -5, 9, 11, 17, 404, 50, 58, 36, 7, 17, 52, 0};
+        int[] expected = {-5, -5, 3, 0, 7, 9, 11, 17, 17, 36, 50, 52, 58, 404};
         Assertions.assertEquals(expected.length,new MergeSort().sort(randNumbers).length);
     }
 }
