6a3b4815a26cffad8776d70d5f85a08cbe02def2
Aleksander Napieralski <aleksander.napieralski@city.ac.uk>
2022-08-12 10:30:14 +0100

    BubbleSort tests implemented.

--- src/test/java/com/sparta/an/InputManagerTest.java
+++
@@ -1,5 +0,0 @@
-package com.sparta.an;
-
-public class InputManagerTest {
-
-}
--- src/test/java/com/sparta/an/sortingAlgorithms/BubbleSortTest.java
+++ src/test/java/com/sparta/an/sortingAlgorithms/BubbleSortTest.java
@@ -1,5 +1,43 @@
 package com.sparta.an.sortingAlgorithms;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 public class BubbleSortTest {
+    @Test
+    @DisplayName("Is sorting in correct numerical order")
+    void isSortingInCorrectNumericalOrder(){
+        int[] randNumbers = {5, 3, 0, 9, 11, 17, 404, 50, 58, 36, 17, 52, 5};
+        int[] expected = {0, 3, 5, 5, 9, 11, 17, 17, 36, 50, 52, 58, 404};
+        Assertions.assertEquals(Arrays.toString(expected),Arrays.toString(new BubbleSort().sort(randNumbers)));
+    }
+
+    @Test
+    @DisplayName("When passing negative values, returns numerical order.")
+    void whenPassingNegativeValuesReturnsNumericalOrder(){
+        int[] randNumbers = {5, -1, -5, 9, 11, 17, 404, 50, 58, 36, 17, 52, 0};
+        int[] expected = {-5, -1, 0, 5, 9, 11, 17, 17, 36, 50, 52, 58, 404};
+        Assertions.assertEquals(Arrays.toString(expected),Arrays.toString(new BubbleSort().sort(randNumbers)));
+    }
+
+    @Test
+    @DisplayName("When passing duplicates, returns sorted array")
+    void whenPassingDuplicatesReturnsSortedArray(){
+        int[] randNumbers = {0, -5, -5, 9, 11, 17, 404, 50, 58, 36, 17, 52, 0};
+        int[] expected = {-5, -5, 0, 0, 9, 11, 17, 17, 36, 50, 52, 58, 404};
+        Assertions.assertEquals(Arrays.toString(expected),Arrays.toString(new BubbleSort().sort(randNumbers)));
+    }
+
+    @Test
+    @DisplayName("When passed an array, returns correct length of the array back")
+    void whenPassedAnArrayReturnsCorrectLengthOfTheArrayBack(){
+        int[] randNumbers = {3, -5, -5, 9, 11, 17, 404, 50, 58, 36, 17, 52, 0};
+        int[] expected = {-5, -5, 3, 0, 9, 11, 17, 17, 36, 50, 52, 58, 404};
+    Assertions.assertEquals(expected.length,new BubbleSort().sort(randNumbers).length);
+    }
+
 
 }
